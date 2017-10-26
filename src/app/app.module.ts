6a5a7dbb86b6a7656e63c985239fddbdebbfdb88
Mehdi Azzabi <mehdi.azzabi.08@gmail.com>
2017-10-26 23:01:09 +0200

    Add form login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { RechercheLogementComponent } from './recherche-logement/recherche-logem
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { LoginComponent } from './login/login.component';
 const appRoutes: Routes =[
   {path :'recherche' , component : RechercheLogementComponent},
   {path :'inscription' , component : InscriptionComponent},
-  {path :'' , redirectTo:'/recherche' , pathMatch : 'full'}
+  {path :'' , redirectTo:'/recherche' , pathMatch : 'full'},
+  {path :'login' , component : LoginComponent},
 ]
 @NgModule({
   declarations: [
     AppComponent,
     RechercheLogementComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(appRoutes), FormsModule
