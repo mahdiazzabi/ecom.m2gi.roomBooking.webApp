b13f3c9330b685e23025d8089a42eca759758115
MAROUF Mohamed Salah <salahmarouf88gmail.com>
2017-12-16 03:45:19 +0100

    Revert "Revert "WIP on master: a18b3f6 details logement""
    This reverts commit ccf75973b259ee184bfba939001224d3ced8dbaf.

--- src/app/logements/logements.component.ts
+++ src/app/logements/logements.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {LogementsServices} from "../../services/logements.service";
-import {Http} from "@angular/http";
+
 import { Logement } from '../../model/model.logement';
 import { Router } from '@angular/router';
+import { Component, OnInit ,Input, SimpleChanges, SimpleChange, Output, EventEmitter} from '@angular/core';
+import {Http} from "@angular/http";
+import { LogementsServices } from '../../services/logements.service';
+import { Recherche } from '../../utils/utils.recherche';
 
 @Component({
   selector: 'app-logements',
@@ -10,7 +12,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./logements.component.css']
 })
 export class LogementsComponent implements OnInit {
-
   listLogement: Array<any> = null;
   currentpage:number=0;
   size:number=4;
@@ -18,28 +19,73 @@ export class LogementsComponent implements OnInit {
   currentLogement:Logement;
   mode:number;
 
-  constructor(public http:Http, public logementsdervice:LogementsServices, public router:Router) { }
+  @Input('recherche') recherche: Recherche;
+  @Input('fromRecherche') fromRecherche: Boolean;
 
+
+  @Output() getSearchStatus = new EventEmitter<Boolean>();
+
+
+  constructor(public http:Http, public logementsService:LogementsServices) { 
+    this.nbrpages= new Array(0);
+    this.mode = 0
+  }
+  
+  ngOnChanges(changes: SimpleChanges) {
+   if (this.fromRecherche) {
+    this.doGetLogementsByVilleDateFromDateTo();
+
+  } else {
+    
+    this.doGetAllLogements();
+    //from espace-hote getLogementByClient...
+  }
+  } 
   ngOnInit() {
-   this.doGet();
-  // this.mode=0;
-  //this.getValueMode(0);
+    if (this.fromRecherche) {
+      this.doGetLogementsByVilleDateFromDateTo();
+
+    } else {
+      
+      this.doGetAllLogements();
+      //from espace-hote getLogementByClient...
+    }
 
   }
 
-  doGet(){
-    this.logementsdervice.getLogements(this.currentpage,this.size)
-      .subscribe(data=>{this.listLogement=data.logemens;
-        this.nbrpages = new Array(data.total);
-        this.mode=0
-      },err=>{console.log(err);})
-  }
+  doGetAllLogements(){
+    
+        this.logementsService.getLogements(this.currentpage,this.size)
+          .subscribe(data=>{this.listLogement=data.logemens;
+            this.nbrpages = new Array(data.total);
+          },err=>{console.log(err);})
+      }
+
+  doGetLogementsByVilleDateFromDateTo(){
+    
+            this.logementsService.getLogementsByRecherche(this.currentpage,this.size,this.recherche)
+              .subscribe(data=>{
+                this.listLogement=data.logemens;
+                this.nbrpages = new Array(data.total);
+                //to notify parent recherche-result 
+                if (this.listLogement.length > 0) {
+                  
+                this.getSearchStatus.emit(true);
+                }else{
+                  
+                this.getSearchStatus.emit(false);
+                }
+              },err=>{
+                console.log(err);
+                //to notify parent recherche-result 
+              })
+          }
 
 
 
   gotoPage(i:number){
     this.currentpage=i;
-    this.doGet();
+    this.doGetAllLogements();
 
   }
 
