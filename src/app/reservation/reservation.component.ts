9a2c6781106b00f116641bd98b183a5c2c04f616
chiraz29 <limayemchiraz7@gmail.com>
2017-12-19 09:24:06 +0100

    mise a jour component reservation(add object reservation, mise a jour methode onsavereservation())

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -18,15 +18,14 @@ export class ReservationComponent implements OnInit {
   private logement: Logement;
   private date: Date;
   private duree:number;
-  newReservation: Reservation ;
   savingErr: any = null;
   constructor(private activatedRoute: ActivatedRoute, private router: Router,private reservationService:ReservationServices) { }
   
   ngOnInit() {
   }
-  get_logement():Logement[]
+  get_logement():Reservation[]
   {
-      let tab_logs:Logement[]= JSON.parse(localStorage.getItem("panier_array_logs"));
+      let tab_logs:Reservation[]= JSON.parse(localStorage.getItem("panier_array_logs"));
       if (tab_logs){ 
         return tab_logs;
       }else {
@@ -37,11 +36,8 @@ export class ReservationComponent implements OnInit {
     this.currentUser = JSON.parse(sessionStorage.getItem("currrentUser"));
      
     console.log(JSON.stringify(this.currentUser));
-    for(let l of this.get_logement()){
-      //this.newReservation = {dateDebut:this.date,  logement:l,client:this.currentUser};  
-        console.log(JSON.stringify(this.newReservation));
-
-        this.reservationService.addReservations(this.newReservation).subscribe(response => {
+    for(let R of this.get_logement()){
+        this.reservationService.addReservations(R).subscribe(response => {
           if (response.err) {
             this.savingErr = response.err;
           } else {
@@ -54,6 +50,6 @@ export class ReservationComponent implements OnInit {
     }
   
 
-}
+  }
 
 }
